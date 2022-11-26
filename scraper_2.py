55582984c2c734cb49df434844bec4229d9f88a6
rakhi902 <74000925+rakhi902@users.noreply.github.com>
2022-11-26 17:59:39 +0530

    Add files via upload

---
+++ scraper_2.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+
+starturl = "https://exoplanets.nasa.gov/discovery/exoplanet-catalog/"
+
+browser = webdriver.Chrome("C:/Users/rakhi/OneDrive/Documents/pythonProjects/Priyanshu/C127/chromedriver.exe")
+browser.get(starturl)
+
+time.sleep(10)
+
+headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date", "hyperlink"]
+planet_data = []
+def scrape():
+    for i in range(1,5):
+        while True:
+            time.sleep(2)
+            soup = BeautifulSoup(browser.page_source, "html.parser")
+
+            #check page number
+            current_page_num = int(soup.find_all("input", attrs = {"class", "page_num"})[0].get("value"))
+        
+            if current_page_num < i:
+                browser.find_element("xpath", '//*[@id="primary_column"]/div[1]/div[2]/div[1]/div/nav/span[2]/a').click()
+            elif current_page_num > i:
+                browser.find_element("xpath", '//*[@id="primary_column"]/div[1]/div[2]/div[1]/div/nav/span[1]/a').click()
+            else:
+                break
+    for ul_tag in soup.find_all("ul", attrs = {"class", "exoplanet"}):
+        li_tags = ul_tag.find_all("li")
+        temp_list = []
+        for index,li_tag in enumerate(li_tags):
+            if index == 0:
+                temp_list.append(li_tag.find_all("a")[0].contents[0])
+            else:
+                try:
+                    temp_list.append(li_tag.contents[0])
+                except:
+                    temp_list.append("")
+        
+        #Get hyperlink tag
+        hyperlink_li_tag = li_tags[0]
+        temp_list.append("https://exoplanets.nasa.gov"+hyperlink_li_tag.find_all("a", href = True)[0]["href"])
+        planet_data.append(temp_list)
+
+    browser.find_element("xpath", '//*[@id="primary_column"]/div[1]/div[2]/div[1]/div/nav/span[2]/a').click()
+    print(f"Page {i} scrapping completed")
+
+    with open("scraper_2.csv","w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(planet_data)
+scrape()
